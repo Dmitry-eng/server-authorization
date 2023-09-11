f1b314c00c3f6663c2a47492df9063eb881cbc2e
ASSA <11dima1996@gmail.com>
2023-09-11 22:32:06 +0300

    fixed

--- src/main/java/com/jwt/server/config/security/SecurityConfig.java
+++ src/main/java/com/jwt/server/config/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
